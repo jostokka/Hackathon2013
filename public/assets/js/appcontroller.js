4514276e0389f61d2489317a4f9099505c7b85f3
jns <jns@TV2>
2013-05-15 20:44:41 +0200

    More stuff

--- public/assets/js/appcontroller.js
+++ public/assets/js/appcontroller.js
@@ -1,15 +1,35 @@
 (function($, window, document, undefined){
 	var AppController = {
 		init: function(options, elem) {
+			console.log('appcontroller')
 			var self = this;
 			self.elem = elem;
 			self.$elem = $(elem);
 			self.guid = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx".replace(/x/g, function() {return Math.floor(Math.random()*16).toString(16).toUpperCase();});
 			self.options = $.extend({}, $.fn.appController.options, options);
 			// load handlebar templates
+			var source   = $("#template-app-home").html();
+			self.template_app_home = Handlebars.compile(source);
 
-			self.fetch().done(function(results){
+			$(self.options.canvas).html(self.template_app_home({}));
+			self.$elem.find('li a').each(function(index, a) {
+				console.log($(a).html())
+				$.data(a, 'appController', self);
+				$(a).on('click', function() {
+					return $(this).appController('loadContent', $(this).attr('template'));
+				}); 
 			});
+			
+			//self.fetch().done(function(results){
+			//});
+		},
+
+      loadContent : function(template) {
+			var self = this;
+			var source   = $("#"+template).html();
+			var tmplate = Handlebars.compile(source);
+			$(self.options.canvas).html(tmplate({}));
+			return false;
 		},
 
 		fetch: function() {
@@ -28,7 +48,7 @@
 	}
 	$.fn.appController = function(method) {
 		// Method calling logic
-		if (this.data('appController') && PlayW[method]) {
+		if (this.data('appController') && AppController[method]) {
 			return AppController[method].apply(this.data('appController'), Array.prototype.slice.call( arguments, 1 ));
 		} else if (typeof method === 'object' || ! method) {
 			return this.each(function(){
@@ -37,12 +57,14 @@
 				$.data(this, 'appController', obj);
 			});
 		} else {
+			console.log(this.data('appController'))
 			$.error( 'Method ' +  method + ' does not exist on jQuery.appController' );
 		}  		
 	};
     
 	$.fn.appController.options = {
-		url: '/rest/'
+		url: '/rest/',
+		canvas: '#content'
 	};
 })(jQuery, window, document);
 
--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -3,12 +3,7 @@
  * and open the template in the editor.
  */
 $( document ).ready(function() {
-  var source   = $("#some-template").html();
-  var template = Handlebars.compile(source);
-  var data = { users: [
-      {username: "alan", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
-      {username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
-      {username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
-    ]};
-  $("#content").html(template(data));
+  jQuery('ul.nav').appController({
+		
+	});
 });
